4b5335ca571a5ca880c6cbedfccae6fcb23b22b4
Henry Liu <henryyin12@gmail.com>
2022-01-17 14:48:16 -0500

    changes

--- newspaper/src/App.js
+++ newspaper/src/App.js
@@ -12,20 +12,6 @@ import Blog3 from "./components/blog3"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <BrowserRouter>
                 <Routes>
                   <Route path="/" element={<Dashboard />}></Route>
